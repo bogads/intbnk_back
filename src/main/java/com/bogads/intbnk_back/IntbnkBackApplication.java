e7f0286235d136cb1d10c18fdf16f5a20c241d31
Sebastian Bogado <sebastian.bogado@itti.digital>
2025-02-14 10:13:36 -0300

    innitial commit

---
+++ src/main/java/com/bogads/intbnk_back/IntbnkBackApplication.java
@@ -0,0 +1,13 @@
+package com.bogads.intbnk_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntbnkBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntbnkBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bogads/intbnk_back/IntbnkBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogads.intbnk_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntbnkBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
